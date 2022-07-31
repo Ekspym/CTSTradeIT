d2c759817a42d6961d279e5ee2b444fc1bc38cd1
TomasNovak <tmasnovak@gmail.com>
2022-07-31 23:14:57 +0200

    Initial commit

---
+++ CTSTradeIT/HtmlCareerReader.cs
@@ -0,0 +1,66 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace CTSTradeIT
+{
+    internal class HtmlCareerReader
+    {
+        private int offersCount = 0;
+        HtmlWeb Careers = new HtmlWeb();
+        HtmlWeb WhatToExpect = new HtmlWeb();
+        List<String> jobOffers = new List<String>();
+        public HtmlCareerReader()
+        {
+
+        }
+
+        /// <summary>
+        /// Metoda na načtení nabídek práce do třídy. Možnost vypsání jobů do commandové řádky.
+        /// </summary>
+        /// <param name="print"></param>
+        /// <returns>Vrací list stringů s názvy pracovních nabídek, pro případ dalšího použítí</returns>
+        public List<String> getAllJobOffers(bool print)
+        {
+
+            HtmlDocument doc = Careers.Load("https://www.cts-tradeit.cz/kariera/");
+            HtmlNodeCollection Nodes = doc.DocumentNode.SelectNodes("//a[@class=\"card card-lg card-link-bottom\"]"); //2x Node. 1 pro název, druhý pro url v downloadWhatToExpectSection()
+            HtmlNodeCollection NodesNamesForUrl = doc.DocumentNode.SelectNodes("//h3[@class=\"card-title mb-0\"]");
+            for (int z = 0; Nodes.Count > z; z++)
+            {
+                string jobOffer = NodesNamesForUrl[z].InnerText;
+                jobOffer = Regex.Replace(jobOffer, @"\s+", " ");
+                string jobOfferForUrl = Nodes[z].Attributes["href"].Value.Remove(0, 9);
+                jobOfferForUrl = jobOfferForUrl.Remove(jobOfferForUrl.Length - 1, 1);
+                jobOffers.Add(jobOfferForUrl);
+                if (print == true)
+                {
+                    Console.WriteLine(z + 1 + "." + jobOffer);
+
+                }
+            }
+          
+            return jobOffers;
+        }
+
+        /// <summary>
+        /// Zapíše sekcí zvoleného jobu pomocí <param name="numberOfJob"/> a podle pořadí v Listu JobOffers
+        /// </summary>
+        /// <param name="numberOfJob"></param>
+        public void downloadWhatToExpectSection(int numberOfJob)
+        {
+            if (Careers.ResponseUri == null)
+                getAllJobOffers(false);
+
+            HtmlDocument doc = Careers.Load("https://www.cts-tradeit.cz/kariera/" + jobOffers[numberOfJob - 1]);
+            HtmlNodeCollection Nodes = doc.DocumentNode.SelectNodes("//div[@class=\"container-9 mb-md-5\"]"); //ul[@class=\"list-check\"] pokud budete chtít jen vše pod "Co tě čeká"
+
+            string htmlValue = Nodes[0].InnerText;
+            htmlValue = Regex.Replace(htmlValue, @"\s+", " "); // Mazání formátování
+
+            File.WriteAllText(jobOffers[numberOfJob - 1] + ".txt", htmlValue);
+        }
+
+    }
+}
+
+
---
+++ CTSTradeIT/Program.cs
@@ -0,0 +1,30 @@
+﻿using CTSTradeIT;
+
+HtmlCareerReader htmlCareerReader = new HtmlCareerReader();
+
+List<string> Careers = htmlCareerReader.getAllJobOffers(true);
+
+Console.WriteLine("Zadejte číslo pracovní nabídky pro stažení obsahu „Co vás na této pozici čeká“.");
+string userInput = Console.ReadLine();
+int numericInput;
+bool isNumeric = int.TryParse(userInput, out numericInput);
+while (!isNumeric || numericInput <=0 || numericInput > Careers.Count)
+{
+    Console.WriteLine("Nezadali jste číslo nebo neodpovídalo označeným pracovním nabídkám.");
+    Console.WriteLine("Zadejte číslo pracovní nabídky pro stažení obsahu „Co vás na této pozici čeká“.");
+     userInput = Console.ReadLine();
+     isNumeric = int.TryParse(userInput, out numericInput);
+}
+htmlCareerReader.downloadWhatToExpectSection(numericInput);
+while(userInput != "")
+{ 
+Console.WriteLine("Pokud chcete stáhnout obsah další pracovní nabídky zadejte odpovídající číslo.");
+ userInput = Console.ReadLine();
+    
+ isNumeric = int.TryParse(userInput, out numericInput);
+    if(isNumeric && numericInput > 0 && numericInput <= Careers.Count)
+ htmlCareerReader.downloadWhatToExpectSection(numericInput);
+    else
+        Console.WriteLine("Nezadali jste číslo nebo neodpovídalo označeným pracovním nabídkám.\nPokud chcete aplikaci ukončit zmáčkněte enter");
+}
+Console.Read();
